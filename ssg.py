41bfc6a95fd9efb163a2c1ac324c5456f31fa116
Ranjbar <homayoon.ranjbar@siemens-healthineers.com>
2022-11-03 17:23:23 -0500

    Initial Commit

---
+++ ssg.py
@@ -0,0 +1,14 @@
+import typer
+from ssg.site import Site
+import ssg.parsers
+
+def main(source="content", dest="dist"):
+    config = {
+    "source": source,
+    "dest": dest,
+    "parsers": [ssg.parsers.ResourceParser(), ssg.parsers.MarkdownParser(), ssg.parsers.ReStructuredTextParser()]
+    }
+    Site(**config).build()
+
+# if __name__ = "__main__":
+typer.run(main)
